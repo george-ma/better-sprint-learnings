c0819c28d8f8404bfd5a9de9d4ff7cce6df3b9ed
George Ma <george.ma@flipp.com>
2019-09-26 11:01:09 -0400

    refactor code

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,4 +1,10 @@
 import React from "react";
-import Routes from "../routes/Index";
+import Routes from "./routes";
 
-export default props => <>{Routes}</>;
+class App extends React.Component {
+  render() {
+    return {Routes}
+  }
+}
+
+export default App;
--- app/javascript/components/packs/Index.jsx
+++ app/javascript/components/packs/Index.jsx
@@ -4,7 +4,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import $ from 'jquery';
 import Popper from 'popper.js';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-import App from "../components/App";
+import App from "../App";
+
+/**
+ * Rails, with the help of the Webpacker gem, bundles all your JavaScript code 
+ * into packs. These can be found in the packs directory at app/javascript/packs.
+ * 
+ */
 
 document.addEventListener("DOMContentLoaded", () => {
   render(
